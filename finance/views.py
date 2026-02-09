50317bbfaeb8ad8a47fe2d1efbbf285e92f7b0d8
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-09 13:47:25 +0500

    make login and register fomr

--- finance/views.py
+++ finance/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from finance.forms import RegisterForm
-# Create your views here.
+from django.contrib.auth import login
+
 class RegisterView(View):
-    def get(self,request,*args, **kwargs):
+    def get(self, request, *args, **kwargs):
         form = RegisterForm()
-        return render(request,'finance/register.html',{'form':form})
-    def post(self,request,*args, **kwargs):
+        return render(request, 'finance/register.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
         form = RegisterForm(request.POST)
-        return render(request,'finance/register.html',{'form':form})
+        if form.is_valid():
+            user = form.save() 
+            login(request, user) 
+            return redirect('home')  
+        return render(request, 'finance/register.html', {'form': form})

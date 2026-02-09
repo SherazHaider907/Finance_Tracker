b3181731baff25aea9ff4010c9604fe510a14681
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-09 17:01:37 +0500

    make transcations list view and file

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from finance.views import RegisterView,DashboardView
+from finance.views import RegisterView,DashboardView,TransactionCreateView,TranscationListView
 urlpatterns = [
     path('register/',RegisterView.as_view(),name="register"),
-    path('',DashboardView.as_view(),name="dashboard")
+    path('',DashboardView.as_view(),name="dashboard"),
+    path('transaction/add/',TransactionCreateView.as_view(),name="transaction_add"),
+    path('transaction',TranscationListView.as_view(),name="transactions_list"),
+
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from finance.forms import RegisterForm
+from finance.forms import RegisterForm,TransactionForm
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
         form = RegisterForm()
@@ -18,3 +19,22 @@ class RegisterView(View):
 class DashboardView(View,LoginRequiredMixin):
     def get(self, request, *args, **kwargs):
         return render(request, 'finance/dashboard.html')
+    
+class TransactionCreateView(View,LoginRequiredMixin):
+    def get(self, request, *args, **kwargs):
+        form = TransactionForm()
+        return render(request, 'finance/transactions.html', {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('dashboard')
+        return render(request, 'finance/transactions.html', {'form': form})     
+    
+class TranscationListView(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+        transaction = Transaction.objects.all()
+        return render(request, 'finance/transactions_list.html',{'transaction':transaction})

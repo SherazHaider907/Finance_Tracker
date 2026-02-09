bbf5a0b85c30dea0e8d40d0577f45a6cc22bdf8e
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-09 12:35:12 +0500

    make form

--- finance/views.py
+++ finance/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from finance.forms import RegisterForm
 # Create your views here.
 class RegisterView(View):
     def get(self,request,*args, **kwargs):
-        return render(request,'finance/register.html')
+        form = RegisterForm()
+        return render(request,'finance/register.html',{'form':form})

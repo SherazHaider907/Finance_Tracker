338cee1c13485526d1b39555df67d7f555cf50ea
SherazHaider907 <Sherazhaider665@gmail.com>
2026-02-09 19:19:08 +0500

    make Gole model

--- finance/urls.py
+++ finance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from finance.views import RegisterView,DashboardView,TransactionCreateView,TranscationListView
+from finance.views import RegisterView,DashboardView,TransactionCreateView,TranscationListView,GoalCreateView
 urlpatterns = [
     path('register/',RegisterView.as_view(),name="register"),
     path('',DashboardView.as_view(),name="dashboard"),
     path('transaction/add/',TransactionCreateView.as_view(),name="transaction_add"),
     path('transaction',TranscationListView.as_view(),name="transactions_list"),
+    path('goal/add/',GoalCreateView.as_view(),name="goal_add"),
 
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from finance.forms import RegisterForm,TransactionForm
+from finance.forms import RegisterForm,TransactionForm,GoalForm
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Transaction
+from .models import Transaction,Goal
+from django.db.models import Sum
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
         form = RegisterForm()
@@ -14,11 +15,40 @@ class RegisterView(View):
         if form.is_valid():
             user = form.save() 
             login(request, user) 
-            redirect('dashboard')  
+            return redirect('login')  
         
-class DashboardView(View,LoginRequiredMixin):
+
+class DashboardView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'finance/dashboard.html')
+        transactions = Transaction.objects.filter(user=request.user)
+        goals = Goal.objects.filter(user=request.user)
+
+        # Calculate total income and total expense
+        total_income = Transaction.objects.filter(user=request.user, transaction_type='Income').aggregate(Sum('amount'))['amount__sum'] or 0
+        total_expense = Transaction.objects.filter(user=request.user, transaction_type='Expense').aggregate(Sum('amount'))['amount__sum'] or 0
+        net_saving = total_income - total_expense
+        remaining_savings = net_saving
+        goal_progress = []
+        for goal in goals:
+            if remaining_savings >=goal.target_amount:
+                goal_progress.append({'goal':goal,'progress':100})
+                remaining_savings -= goal.target_amount
+            elif remaining_savings > 0:
+                progress = (remaining_savings/goal.target_amount) * 100
+                goal_progress.append({'goal':goal,'progress':progress})
+                remaining_savings = 0
+            else:
+                goal_progress.append({'goal':goal,'progress':0})
+        context = {
+            'transactions': transactions,
+            'goals': goals,
+            'total_income': total_income,
+            'total_expense': total_expense,
+            'net_saving': net_saving,
+            'goal_progress':goal_progress
+        }
+
+        return render(request, 'finance/dashboard.html', context)
     
 class TransactionCreateView(View,LoginRequiredMixin):
     def get(self, request, *args, **kwargs):
@@ -36,5 +66,22 @@ class TransactionCreateView(View,LoginRequiredMixin):
     
 class TranscationListView(LoginRequiredMixin,View):
     def get(self, request, *args, **kwargs):
-        transaction = Transaction.objects.all()
+        transaction = Transaction.objects.filter(user=request.user)
         return render(request, 'finance/transactions_list.html',{'transaction':transaction})
+    
+
+
+class GoalCreateView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = GoalForm()
+        return render(request, 'finance/goal_form.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = GoalForm(request.POST)
+        if form.is_valid():
+            goal = form.save(commit=False)
+            goal.user = request.user
+            goal.save()
+            return redirect('dashboard')
+        return render(request, 'finance/goal_form.html', {'form': form})    
+    
